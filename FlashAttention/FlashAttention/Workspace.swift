861b347051ede0e644ea3784da6381b5556319c6
Philip Turner <philipturner.AR@gmail.com>
2024-06-24 15:47:05 -0400

    Refactored the code into a data structure

--- FlashAttention/FlashAttention/Workspace.swift
+++ FlashAttention/FlashAttention/Workspace.swift
@@ -66,111 +66,12 @@ func executeScript() {
   let N: Int = 10
   let D: Int = 3
   
-  // Randomly initialize Q, K, V, C ∈ R^{NxD}
-  var Q = [Float](repeating: .zero, count: N * D)
-  var K = [Float](repeating: .zero, count: N * D)
-  var V = [Float](repeating: .zero, count: N * D)
-  var C = [Float](repeating: .zero, count: N * D)
+  var networkDesc = NetworkDescriptor()
+  networkDesc.N = N
+  networkDesc.D = D
+  let network = Network(descriptor: networkDesc)
   
-  func boxMullerTransform() -> SIMD2<Float> {
-    let randomUniform = SIMD2<Float>.random(in: 0..<1)
-    let logPart = Float.log(randomUniform[0])
-    let magnitudePart = (-2 * logPart).squareRoot()
-    
-    // This is an inefficient way to compute trigonometric quantities, but
-    // we're not particularly focused on efficiency here.
-    let anglePart = 2 * Float.pi * randomUniform[1]
-    let cosPart = Float.cos(anglePart)
-    let sinPart = Float.sin(anglePart)
-    
-    return SIMD2(
-      magnitudePart * cosPart,
-      magnitudePart * sinPart)
-  }
-  
-  for n in 0..<N {
-    for d in 0..<D {
-      let matrixAddress = n * D + d
-      let randomQK = boxMullerTransform()
-      let randomVC = boxMullerTransform()
-      Q[matrixAddress] = randomQK[0]
-      K[matrixAddress] = randomQK[1]
-      V[matrixAddress] = randomVC[0]
-      C[matrixAddress] = randomVC[1]
-    }
-  }
-  
-  // Forward attention layer:
-  // - S = QK^T
-  // - P = softmax(S)
-  // - O = PV
-  var O = [Float](repeating: .zero, count: N * D)
-  
-  print()
-  print("attention matrix:")
-  for rowID in 0..<N {
-    var attentionMatrixRow = [Float](repeating: .zero, count: N)
-    
-    // Q * K
-    for columnID in 0..<N {
-      var dotProduct: Float = .zero
-      for d in 0..<D {
-        let addressQ = rowID * D + d
-        let addressK = columnID * D + d
-        dotProduct += Q[addressQ] * K[addressK]
-      }
-      attentionMatrixRow[columnID] = dotProduct
-    }
-    
-    // softmax
-    do {
-      var maximum: Float = -.greatestFiniteMagnitude
-      for columnID in 0..<N {
-        let value = attentionMatrixRow[columnID]
-        maximum = max(maximum, value)
-      }
-      
-      var sum: Float = .zero
-      for columnID in 0..<N {
-        let value = attentionMatrixRow[columnID]
-        let expTerm = Float.exp(value - maximum)
-        sum += expTerm
-      }
-      
-      for columnID in 0..<N {
-        let value = attentionMatrixRow[columnID]
-        let expTerm = Float.exp(value - maximum)
-        attentionMatrixRow[columnID] = expTerm / sum
-        
-        // Display the attention matrix.
-        let matrixValue = attentionMatrixRow[columnID]
-        var repr = String(format: "%.3f", matrixValue)
-        while repr.count < 8 {
-          repr = " " + repr
-        }
-        print(repr, terminator: " ")
-      }
-      print()
-    }
-    
-    // P * V
-    var outputMatrixRow = [Float](repeating: .zero, count: D)
-    for d in 0..<D {
-      var dotProduct: Float = .zero
-      for columnID in 0..<N {
-        let attentionMatrixValue = attentionMatrixRow[columnID]
-        let addressV = columnID * D + d
-        dotProduct += attentionMatrixValue * V[addressV]
-      }
-      outputMatrixRow[d] = dotProduct
-    }
-    
-    for d in 0..<D {
-      let outputMatrixValue = outputMatrixRow[d]
-      let addressO = rowID * D + d
-      O[addressO] = outputMatrixValue
-    }
-  }
+  let O = network.inferenceAttention()
   
   // Displays a matrix with dimensions N * d.
   func printMatrix(_ matrix: [Float]) {
@@ -190,15 +91,15 @@ func executeScript() {
   
   print()
   print("Q^T:")
-  printMatrix(Q)
+  printMatrix(network.Q)
   
   print()
   print("K^T:")
-  printMatrix(K)
+  printMatrix(network.K)
   
   print()
   print("V^T:")
-  printMatrix(V)
+  printMatrix(network.V)
   
   print()
   print("O^T:")
@@ -206,21 +107,164 @@ func executeScript() {
   
   print()
   print("C^T:")
-  printMatrix(C)
+  printMatrix(network.C)
   
-  // Loss function layer:
-  // - Φ = Σ_n Σ_d C[n][d] * O[n][d]
-  var Φ: Float = .zero
-  for n in 0..<1 {
-    for d in 0..<D {
-      let address = n * D + d
-      Φ += O[address] * C[address]
-    }
-  }
+  let Φ = network.loss(O: O)
   print()
   print("Φ:", Φ)
   
   // TODO: Wrap the above code in a function, which accepts Q/K/V/O as
   // (descriptor) arguments. Use it as a primitive for finite differencing.
   // Show the limit of the derivative for each variable as h approaches 0.
+}
+
+struct NetworkDescriptor {
+  var N: Int?
+  var D: Int?
+}
+
+struct Network {
+  let N: Int
+  let D: Int
+  
+  var Q: [Float]
+  var K: [Float]
+  var V: [Float]
+  var C: [Float]
+  
+  init(descriptor: NetworkDescriptor) {
+    guard let N = descriptor.N,
+          let D = descriptor.D else {
+      fatalError("Descriptor was incomplete.")
+    }
+    self.N = N
+    self.D = D
+    
+    // Randomly initialize Q, K, V, C ∈ R^{NxD}
+    Q = [Float](repeating: .zero, count: N * D)
+    K = [Float](repeating: .zero, count: N * D)
+    V = [Float](repeating: .zero, count: N * D)
+    C = [Float](repeating: .zero, count: N * D)
+    
+    for n in 0..<N {
+      for d in 0..<D {
+        let matrixAddress = n * D + d
+        let randomQK = Network.boxMullerTransform()
+        let randomVC = Network.boxMullerTransform()
+        Q[matrixAddress] = randomQK[0]
+        K[matrixAddress] = randomQK[1]
+        V[matrixAddress] = randomVC[0]
+        C[matrixAddress] = randomVC[1]
+      }
+    }
+  }
+  
+  static func boxMullerTransform() -> SIMD2<Float> {
+    let randomUniform = SIMD2<Float>.random(in: 0..<1)
+    let logPart = Float.log(randomUniform[0])
+    let magnitudePart = (-2 * logPart).squareRoot()
+    
+    // This is an inefficient way to compute trigonometric quantities, but
+    // we're not particularly focused on efficiency here.
+    let anglePart = 2 * Float.pi * randomUniform[1]
+    let cosPart = Float.cos(anglePart)
+    let sinPart = Float.sin(anglePart)
+    
+    return SIMD2(
+      magnitudePart * cosPart,
+      magnitudePart * sinPart)
+  }
+}
+
+extension Network {
+  // Performs self-attention with the current values of Q, K, and V.
+  // - S = QK^T
+  // - P = softmax(S)
+  // - O = PV
+  //
+  // Returns O, a matrix with dimensions N * D.
+  func inferenceAttention() -> [Float] {
+    var output = [Float](repeating: .zero, count: N * D)
+    
+    print()
+    print("attention matrix:")
+    for rowID in 0..<N {
+      var attentionMatrixRow = [Float](repeating: .zero, count: N)
+      
+      // Q * K
+      for columnID in 0..<N {
+        var dotProduct: Float = .zero
+        for d in 0..<D {
+          let addressQ = rowID * D + d
+          let addressK = columnID * D + d
+          dotProduct += Q[addressQ] * K[addressK]
+        }
+        attentionMatrixRow[columnID] = dotProduct
+      }
+      
+      // softmax
+      do {
+        var maximum: Float = -.greatestFiniteMagnitude
+        for columnID in 0..<N {
+          let value = attentionMatrixRow[columnID]
+          maximum = max(maximum, value)
+        }
+        
+        var sum: Float = .zero
+        for columnID in 0..<N {
+          let value = attentionMatrixRow[columnID]
+          let expTerm = Float.exp(value - maximum)
+          sum += expTerm
+        }
+        
+        for columnID in 0..<N {
+          let value = attentionMatrixRow[columnID]
+          let expTerm = Float.exp(value - maximum)
+          attentionMatrixRow[columnID] = expTerm / sum
+          
+          // Display the attention matrix.
+          let matrixValue = attentionMatrixRow[columnID]
+          var repr = String(format: "%.3f", matrixValue)
+          while repr.count < 8 {
+            repr = " " + repr
+          }
+          print(repr, terminator: " ")
+        }
+        print()
+      }
+      
+      // P * V
+      var outputMatrixRow = [Float](repeating: .zero, count: D)
+      for d in 0..<D {
+        var dotProduct: Float = .zero
+        for columnID in 0..<N {
+          let attentionMatrixValue = attentionMatrixRow[columnID]
+          let addressV = columnID * D + d
+          dotProduct += attentionMatrixValue * V[addressV]
+        }
+        outputMatrixRow[d] = dotProduct
+      }
+      
+      for d in 0..<D {
+        let outputMatrixValue = outputMatrixRow[d]
+        let addressO = rowID * D + d
+        output[addressO] = outputMatrixValue
+      }
+    }
+    
+    return output
+  }
+  
+  // Computes the loss from the stored value of C, and the entered value of O.
+  // - Φ = Σ_n Σ_d C[n][d] * O[n][d]
+  func loss(O: [Float]) -> Float {
+    var output: Float = .zero
+    for n in 0..<N {
+      for d in 0..<D {
+        let address = n * D + d
+        output += C[address] * O[address]
+      }
+    }
+    return output
+  }
 }
