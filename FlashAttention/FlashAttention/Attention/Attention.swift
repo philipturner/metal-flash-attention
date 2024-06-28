3b6be88e3578fe239141d67c90128a1a85fec784
Philip Turner <philipturner.AR@gmail.com>
2024-06-28 09:50:15 -0400

    Coded the initialization procedure for the forward kernel

--- FlashAttention/FlashAttention/Attention/Attention.swift
+++ FlashAttention/FlashAttention/Attention/Attention.swift
@@ -66,16 +66,88 @@ struct AttentionDescriptor {
   /// Entering 'H' (or any other info about the data's layout in memory) through
   /// a buffer table argument at runtime will seriously harm performance.
   var matrixDimensions: (R: UInt32, C: UInt32, D: UInt16)?
+  
+  /// Whether each operand is transposed in RAM.
+  ///
+  /// If the layout is row-major, where a row spans D contiguous elements in
+  /// memory, enter `false`. If the layout is column-major, where a row spans
+  /// D widely separated elements in memory, enter `true`.
+  ///
+  /// The transpose state of the corresponding derivative (e.g. dQ for Q) must
+  /// be the same as the input from the forward pass.
+  var transposeState: (Q: Bool, K: Bool, V: Bool, O: Bool)?
 }
 
 func createForwardAttention(descriptor: AttentionDescriptor) -> String {
-  guard let (R, C, D) = descriptor.matrixDimensions else {
+  guard let matrixDimensions = descriptor.matrixDimensions,
+        let transposeState = descriptor.transposeState else {
     fatalError("Descriptor was incomplete.")
   }
+  var source: String = ""
   
-  let paddedD = (D + 8 - 1) / 8 * 8
+  // Inject the contents of the headers.
+  source += """
+\(createMetalSimdgroupEvent())
+\(createMetalSimdgroupMatrixStorage())
+using namespace metal;
+
+"""
   
-  return """
+  // Declare the size of the register allocation.
+  let paddedD = (matrixDimensions.D + 8 - 1) / 8 * 8
+  
+  // Determine the block dimensions from the transpose state.
+  var leadingDimensionQ: String = "D"
+  var leadingDimensionK: String = "D"
+  var leadingDimensionV: String = "D"
+  var leadingDimensionO: String = "D"
+  var leadingBlockDimensionQ: UInt16 = paddedD
+  var leadingBlockDimensionK: UInt16 = paddedD
+  var leadingBlockDimensionV: UInt16 = paddedD
+  var leadingBlockDimensionO: UInt16 = paddedD
+  if transposeState.Q {
+    leadingDimensionQ = "R"
+    leadingBlockDimensionQ = 32
+  }
+  if transposeState.K {
+    leadingDimensionK = "C"
+    leadingBlockDimensionK = 32
+  }
+  if transposeState.V {
+    leadingDimensionV = "C"
+    leadingBlockDimensionV = 32
+  }
+  if transposeState.O {
+    leadingDimensionO = "R"
+    leadingBlockDimensionO = 32
+  }
+  
+  // Add the function constants.
+  do {
+    source += """
+
+// Dimensions of each matrix.
+constant uint R = \(matrixDimensions.R);
+constant uint C = \(matrixDimensions.C);
+constant uint D = \(matrixDimensions.D);
+
+// Whether each matrix is transposed.
+constant bool Q_trans = \(transposeState.Q);
+constant bool K_trans = \(transposeState.K);
+constant bool V_trans = \(transposeState.V);
+constant bool O_trans = \(transposeState.O);
+
+// Define the memory layout of the matrix block.
+constant ushort R_group = 32;
+constant ushort C_group = 32;
+
+"""
+  }
+  
+  // Add the setup portion where the addresses are prepared.
+  do {
+    source += """
+
 // What are the Metal function arguments?
 // - Q [FP32, FP16]
 // - K [FP32, FP16]
@@ -91,7 +163,7 @@ kernel void forward(
   device float *O [[buffer(3)]],
   device float *softmax_logsumexp [[buffer(4)]],
   
-  threadgroup uchar [[threadgroup(0)]],
+  threadgroup uchar *threadgroup_block [[threadgroup(0)]],
   
   uint gid [[threadgroup_position_in_grid]],
   ushort sidx [[simdgroup_index_in_threadgroup]],
@@ -105,15 +177,60 @@ kernel void forward(
   // - m accumulator
   // - l accumulator
   // - location where SIMD 0 will async copy from
-
-  simdgroup_matrix_storage<float> Q_sram[\(paddedD / 8)];
-  simdgroup_matrix_storage<float> O_sram[\(paddedD / 8)];
+  
+  // Prefetch the Q block.
+  auto Q_block = (threadgroup float*)threadgroup_block;
+  if (sidx == 0) {
+    uint2 Q_offset(0, gid * R_group);
+    auto Q_src = simdgroup_matrix_storage<float>::apply_offset(
+      Q, \(leadingDimensionQ), Q_offset, Q_trans);
+    
+    ushort R_tile_dimension = min(uint(R_group), R - Q_offset.y);
+    ushort2 Q_tile_src(D, R_tile_dimension);
+    ushort2 Q_tile_dst(\(paddedD), R_tile_dimension);
+    
+    simdgroup_event event;
+    event.async_copy(Q_block, \(leadingBlockDimensionQ), Q_tile_dst,
+                     Q_src, \(leadingDimensionQ), Q_tile_src, Q_trans);
+    simdgroup_event::wait(1, &event);
+  }
+  
+  // Initialize the accumulator.
   float m = -numeric_limits<float>::max();
   float l = numeric_limits<float>::denorm_min();
+  simdgroup_matrix_storage<float> O_sram[\(paddedD / 8)];
+#pragma clang loop unroll(full)
+  for (ushort d = 0; d < \(paddedD); d += 8) {
+    O_sram[d / 8] = simdgroup_matrix_storage<float>(0);
+  }
   
+  // Load the Q block.
+  threadgroup_barrier(mem_flags::mem_threadgroup);
+  simdgroup_matrix_storage<float> Q_sram[\(paddedD / 8)];
+  {
+    ushort2 Q_offset = ushort2(0, sidx * 8) + morton_offset;
+    auto Q_src = simdgroup_matrix_storage<float>::apply_offset(
+      Q_block, \(leadingBlockDimensionQ), Q_offset, Q_trans);
+    
+    for (ushort d = 0; d < \(paddedD); d += 8) {
+      ushort2 origin(d, 0);
+      Q_sram[d / 8].load(Q_block, \(leadingBlockDimensionQ), origin, Q_trans);
+    }
+  }
+
+  for (ushort c = 0; c < C; c += C_group) {
+
+  }
+  
+  // TODO: Compile the kernel and fix the related issues before moving on.
+  // Perhaps write the initialization procedure for the backward kernels, and
+  // find where code can be shared.
 }
 
 """
+  }
+  
+  return source
 }
 
 func createBackwardQueryAttention() -> String {
--- FlashAttention/FlashAttention/Workspace.swift
+++ FlashAttention/FlashAttention/Workspace.swift
@@ -14,7 +14,22 @@ import QuartzCore
 /// when the 'main' branch is in a stable state. Clients can utilize this
 /// function to script tests in their fork.
 func executeScript() {
+  // Deferring profiling against naive attention to a later date (it must be
+  // done for accurate insights into performance). For now, working on the
+  // greatest bottleneck: getting any test at all, of attention being done
+  // entirely on the GPU.
   print("Hello, console.")
+  
+  var attentionDesc = AttentionDescriptor()
+  attentionDesc.matrixDimensions = (100, 150, 30)
+  attentionDesc.transposeState = (false, false, false, false)
+  let forwardKernelSource = createForwardAttention(descriptor: attentionDesc)
+  
+  let device = MTLContext.global.device
+  let library = try! device.makeLibrary(
+    source: forwardKernelSource, options: nil)
+  let function = library.makeFunction(name: "forward")!
+  _ = try! device.makeComputePipelineState(function: function)
 }
 
 
