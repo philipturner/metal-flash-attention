c4bdb422796b20f82436a4f729189b18a998815e
Philip Turner <philipturner.AR@gmail.com>
2024-07-02 08:46:29 -0400

    Drafted the remainder of the forward kernel

--- FlashAttention/FlashAttention/Attention/AttentionKernel.swift
+++ FlashAttention/FlashAttention/Attention/AttentionKernel.swift
@@ -21,6 +21,7 @@ struct AttentionKernel {
     Q: String, K: String, V: String, O: String)
   private var leadingBlockDimensions: (
     Q: UInt16, K: UInt16, V: UInt16, O: UInt16)
+  private var matrixDimensionD: UInt16
   private var memoryPrecisions: (
     Q: AttentionOperandPrecision,
     K: AttentionOperandPrecision,
@@ -37,6 +38,7 @@ struct AttentionKernel {
           let type = descriptor.type else {
       fatalError("Descriptor was incomplete.")
     }
+    self.matrixDimensionD = matrixDimensions.D
     self.memoryPrecisions = memoryPrecisions
     self.transposeState = transposeState
     
@@ -423,7 +425,8 @@ extension AttentionKernel {
       if computeL {
         output += """
 
-  float L_term = fma(fast::log2(l), M_LN2_F, m);
+  // Premultiplied by M_LOG2E_F.
+  float L_term = fast::log2(l) * M_LOG2E_F + m;
   L_terms[linear_array_slot] = L_term;
 
 """
@@ -810,6 +813,9 @@ extension AttentionKernel {
       prefetchV = prefetchRows(descriptor: accessDesc)
     }
     
+    var scaleFactor = Float(1.44269504089) // M_LOG2E_F
+    scaleFactor /= Float(matrixDimensionD).squareRoot()
+    
     return """
   
   // Iterate over the columns.
@@ -827,19 +833,14 @@ extension AttentionKernel {
     simdgroup_matrix_storage<float> S_sram[32 / 8];
 #pragma clang loop unroll(full)
     for (ushort d = 0; d < \(paddedD); d += 8) {
-      simdgroup_matrix_storage<float> KT_sram[32 / 8];
 #pragma clang loop unroll(full)
       for (ushort c = 0; c < 32; c += 8) {
         ushort2 origin(c, d);
-        KT_sram[c / 8].load(
+        simdgroup_matrix_storage<float> KT;
+        KT.load(
           KT_block, \(leadingBlockDimensions.K), origin, \(!transposeState.K));
-      }
-#pragma clang loop unroll(full)
-      for (ushort c = 0; c < 32; c += 8) {
-        auto Q = Q_sram[d / 8];
-        auto KT = KT_sram[c / 8];
-        auto S = S_sram[c / 8];
-        S->multiply(*Q, *KT, d > 0);
+        S_sram[c / 8]
+          .multiply(Q_sram[d / 8], KT, d > 0);
       }
     }
 
@@ -847,10 +848,23 @@ extension AttentionKernel {
     if ((C % 32 != 0) && (c + 32 > C)) {
       const ushort remainder32 = C - uint(C % 32);
       const ushort remainder8 = C - uint(C % 8);
-
+      const ushort remainder32_floor = remainder32 - ushort(remainder32 % 8);
+      
+#pragma clang loop unroll(full)
+      for (ushort index = 0; index < 2; ++index) {
+        if (offset_in_simd.x + index >= remainder32 - remainder32_floor) {
+          auto S_elements = S_sram[remainder32_floor / 8].thread_elements();
+          *(S_elements)[index] = -numeric_limits<float>::max();
+        }
+      }
+#pragma clang loop unroll(full)
+      for (ushort c = remainder32 - remainder32_floor; c < 32; c += 8) {
+        auto S_elements = S_sram[c / 8].thread_elements();
+        *(S_elements) = -numeric_limits<float>::max();
+      }
     }
     
-    // (m, l, P) = softmax(m, l, S * scaleFactor)
+    // update 'm'
     float2 m_new_accumulator;
 #pragma clang loop unroll(full)
     for (ushort c = 0; c < 32; c += 8) {
@@ -861,7 +875,68 @@ extension AttentionKernel {
         m_new_accumulator = max(m_new_accumulator, *S_elements);
       }
     }
-    float m_new
+    float m_new = max(m_new_accumulator[0], m_new_accumulator[1]);
+    m_new = max(m_new, simd_shuffle_xor(m_new, 1));
+    m_new = max(m_new, simd_shuffle_xor(m_new, 8));
+    m_new *= \(scaleFactor);
+
+    // update the previous value of 'O'
+    float correction = 1;
+    if (m_new > m) {
+      correction = fast::exp2(m - m_new);
+#pragma clang loop unroll(full)
+      for (ushort d = 0; d < \(paddedD); d += 8) {
+        *(O_sram[d / 8].thread_elements()) *= correction;
+      }
+      m = m_new;
+    }
+
+    // P = softmax(S * scaleFactor)
+    simdgroup_matrix_storage<float> P_sram[32 / 8];
+#pragma clang loop unroll(full)
+    for (ushort c = 0; c < 32; c += 8) {
+      float2 S_elements = float2(*(S_sram[c / 8].thread_elements()));
+      float2 P_elements = fast::exp2(S_elements * \(scaleFactor) - m);
+      *(P_sram[c / 8].thread_elements()) = P_elements;
+    }
+
+    // update 'l'
+    float2 l_new_accumulator;
+#pragma clang loop unroll(full)
+    for (ushort c = 0; c < 32; c += 8) {
+      auto P_elements = P_sram[c / 8].thread_elements();
+      if (c == 0) {
+        l_new_accumulator = *P_elements;
+      } else {
+        l_new_accumulator += *P_elements;
+      }
+    }
+    float l_new = l_new_accumulator[0] + l_new_accumulator[1];
+    l_new += simd_shuffle_xor(l_new, 1);
+    l_new += simd_shuffle_xor(l_new, 8);
+    l = l * correction + l_new;
+    
+    // load V[c]
+    threadgroup_barrier(mem_flags::mem_threadgroup);
+    \(prefetchV)
+    auto V_block = (threadgroup float*)(threadgroup_block);
+    V_block = simdgroup_matrix_storage<float>::apply_offset(
+      V_block, \(leadingBlockDimensions.V), morton_offset, \(transposeState.V));
+    
+    // O += P * V
+    threadgroup_barrier(mem_flags::mem_threadgroup);
+#pragma clang loop unroll(full)
+    for (ushort c = 0; c < 32; c += 8) {
+#pragma clang loop unroll(full)
+      for (ushort d = 0; d < \(paddedD); d += 8) {
+        ushort2 origin(d, c);
+        simdgroup_matrix_storage<float> V;
+        V.load(
+          V_block, \(leadingBlockDimensions.V), origin, \(transposeState.V));
+        O_sram[d / 8]
+          .multiply(P_sram[c / 8], V, true);
+      }
+    }
   }
   
   // O /= l
